f2cd6705bd0b9d47729b1a27450e7de2da57c338
Abdelrahmanmagdy19 <abdomagdy12393@gmail.com>
2024-12-30 13:41:31 +0200

    create cutom ntoe icon

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -11,10 +12,9 @@ class NotesViewBody extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 24),
         child: Column(
           children: [
-            SizedBox(
-              height: 25,
-            ),
-            CustomAppBar()
+            const SizedBox(height: 25),
+            const CustomAppBar(),
+            NoteItem()
           ],
         ),
       ),
